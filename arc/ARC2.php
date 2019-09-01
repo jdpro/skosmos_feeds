c01f1c768624ea735450a91f3bdd75f14d09f523
joachim.dornbusch@ehess.fr <joachim.dornbusch@ehess.fr>
2019-09-01 08:59:43 +0200

    Fix Arc2 dependency issue when installing with Composer

--- arc/ARC2.php
+++ arc/ARC2.php
@@ -1,4 +1,5 @@
 <?php
-$path = __DIR__ . '/../vendor/semsol';
-set_include_path(get_include_path() . PATH_SEPARATOR . $path);
+$devPath = __DIR__ . '/../vendor/semsol';
+$path = Drupal::root().'/../vendor/semsol';
+set_include_path(get_include_path() . PATH_SEPARATOR . $devPath. PATH_SEPARATOR . $path);
 require_once 'arc2/ARC2.php';
