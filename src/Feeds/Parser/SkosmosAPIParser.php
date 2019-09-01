897ab08b7212da03834298493dc93b737f38b6b6
joachim.dornbusch@ehess.fr <joachim.dornbusch@ehess.fr>
2019-09-01 10:21:34 +0200

    Restrict generated terms by following 'narrower' relations.

--- src/Feeds/Parser/SkosmosAPIParser.php
+++ src/Feeds/Parser/SkosmosAPIParser.php
@@ -18,132 +18,130 @@ use Drupal\skosmos_feeds\Feeds\Item\SkosConceptItem;
  *   title = "Skos RDF",
  *   description = @Translation("Parses RDF data from Skosmos API."),
  *   form = {
- *     "configuration" = "Drupal\skosmos_feeds\Feeds\Parser\Form\SkosmosAPIParserForm",
- *     "feed" = "Drupal\skosmos_feeds\Feeds\Parser\Form\SkosmosAPIParserFeedForm",
+ *     "configuration" =
+ *   "Drupal\skosmos_feeds\Feeds\Parser\Form\SkosmosAPIParserForm",
+ *     "feed" =
+ *   "Drupal\skosmos_feeds\Feeds\Parser\Form\SkosmosAPIParserFeedForm",
  *   },
  *   arguments = {"@skosmos_feeds.rdf_graph_service"}
  * )
  */
-class SkosmosAPIParser extends ParserBase
-{
+class SkosmosAPIParser extends ParserBase {
 
-    /**
-     * SkosmosAPIParser constructor.
-     * @param array $configuration
-     * @param $plugin_id
-     * @param array $plugin_definition
-     * @param \Drupal\skosmos_feeds\Model\RdfGraphService $rdfGraphService
-     */
-    public function __construct(array $configuration, $plugin_id, array $plugin_definition, \Drupal\skosmos_feeds\Model\RdfGraphService $rdfGraphService)
-    {
-        $this->rdfGraphService = $rdfGraphService;
-        parent::__construct($configuration, $plugin_id, $plugin_definition);
+  /**
+   * SkosmosAPIParser constructor.
+   *
+   * @param array $configuration
+   * @param $plugin_id
+   * @param array $plugin_definition
+   * @param \Drupal\skosmos_feeds\Model\RdfGraphService $rdfGraphService
+   */
+  public function __construct(array $configuration, $plugin_id, array $plugin_definition, \Drupal\skosmos_feeds\Model\RdfGraphService $rdfGraphService) {
+    $this->rdfGraphService = $rdfGraphService;
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function parse(FeedInterface $feed, FetcherResultInterface $fetcher_result, StateInterface $state) {
+    $feed_config = $feed->getConfigurationFor($this);
+
+    if (!filesize($fetcher_result->getFilePath())) {
+      throw new EmptyFeedException();
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function parse(FeedInterface $feed, FetcherResultInterface $fetcher_result, StateInterface $state)
-    {
-        $feed_config = $feed->getConfigurationFor($this);
+    // Load and configure parser.
+    $concepts = $this->rdfGraphService->findConcepts($fetcher_result->getFilePath(), $state);
+    if (FALSE === $concepts) {
+      throw new EmptyFeedException();
+    }
+    $result = new ParserResult();
+    $state->total = count($concepts);
+    $counter = 0;
 
-        if (!filesize($fetcher_result->getFilePath())) {
-            throw new EmptyFeedException();
-        }
+    foreach ($concepts as $concept) {
+      // Report progress.
+      $counter += 1;
+      //      $state->pointer = $counter;
+      //      $state->progress($state->total, $state->pointer);
 
-        // Load and configure parser.
-        $concepts = $this->rdfGraphService->findConcepts($fetcher_result->getFilePath());
+      $item = new SkosConceptItem();
+      $prefLabel = $concept->getLiteral('skos:prefLabel');
+      $broader = $concept->getResource('skos:broader');
+      $item->set('prefLabel', $prefLabel->getValue());
+      $item->set('URI', $concept->getUri());
+      if ($broader instanceof \EasyRdf_Resource) {
+        $item->set('broader', $broader->getUri());
+      }
+      $result->addItem($item);
 
-        $result = new ParserResult();
-        $state->total = count($concepts);
-        $counter = 0;
-
-        foreach ($concepts as $concept) {
-            // Report progress.
-            $counter += 1;
-            $state->pointer = $counter;
-            $state->progress($state->total, $state->pointer);
-
-            $item = new SkosConceptItem();
-            $prefLabel = $concept->getLiteral('skos:prefLabel');
-            $broader = $concept->getResource('skos:broader');
-            $item->set('prefLabel', $prefLabel->getValue());
-            $item->set('URI', $concept->getUri());
-            if ($broader instanceof \EasyRdf_Resource) {
-                $item->set('broader', $broader->getUri());
-            }
-            $result->addItem($item);
-
-            $state->setMessage("Parsing concept {$prefLabel}");
-            $state->logMessages($feed);
-        }
-
-
-        $state->setCompleted();
-
-
-        return $result;
+      $state->setMessage("Parsing concept {$prefLabel}");
+      $state->logMessages($feed);
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function getMappingSources()
-    {
-        return [
-            'prefLabel' => [
-                'label' => $this->t('skos:prefLabel'),
-                'description' => $this->t('Preferred label of the concept.'),
-                // todo REMOVE
-                'suggestions' => [
-                    'targets' => ['subject', 'title', 'label', 'name'],
-                    'types' => [
-                        'field_item:text' => [],
-                    ],
-                ],
-            ],
-            'URI' => [
-                'label' => $this->t('Resource URI'),
-                'description' => $this->t('URI of the RDF resource.'),
-            ],
-            'broader' => [
-                'label' => $this->t('skos:broader'),
-                'description' => $this->t('Broader concept.'),
-            ],
-        ];
-    }
 
-    /**
-     * {@inheritdoc}
-     */
-    protected function configSourceLabel()
-    {
-        return $this->t('Skos Concept source');
-    }
+    //    $state->setCompleted();
 
-    /**
-     * {@inheritdoc}
-     */
-    public function defaultFeedConfiguration()
-    {
-        // TODO remove
-        return [
-            'delimiter' => $this->configuration['delimiter'],
-            'no_headers' => $this->configuration['no_headers'],
-        ];
-    }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function defaultConfiguration()
-    {
-        // TODO remove
-        return [
-            'delimiter' => ',',
-            'no_headers' => 0,
-            'line_limit' => 100,
-        ];
-    }
+    return $result;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getMappingSources() {
+    return [
+      'prefLabel' => [
+        'label' => $this->t('skos:prefLabel'),
+        'description' => $this->t('Preferred label of the concept.'),
+        // todo REMOVE
+        'suggestions' => [
+          'targets' => ['subject', 'title', 'label', 'name'],
+          'types' => [
+            'field_item:text' => [],
+          ],
+        ],
+      ],
+      'URI' => [
+        'label' => $this->t('Resource URI'),
+        'description' => $this->t('URI of the RDF resource.'),
+      ],
+      'broader' => [
+        'label' => $this->t('skos:broader'),
+        'description' => $this->t('Broader concept.'),
+      ],
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function configSourceLabel() {
+    return $this->t('Skos Concept source');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultFeedConfiguration() {
+    // TODO remove
+    return [
+      'delimiter' => $this->configuration['delimiter'],
+      'no_headers' => $this->configuration['no_headers'],
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    // TODO remove
+    return [
+      'delimiter' => ',',
+      'no_headers' => 0,
+      'line_limit' => 100,
+    ];
+  }
 
 }
--- src/Model/RdfGraphService.php
+++ src/Model/RdfGraphService.php
@@ -11,192 +11,221 @@ use EasyRdf_Serialiser_Arc;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\Core\StringTranslation\TranslationInterface;
 
-class RdfGraphService
-{
+class RdfGraphService {
 
-    use StringTranslationTrait;
-    const PROGRESS_TOTAL = 1000;
+  use StringTranslationTrait;
 
-    /**
-     * The cache backend.
-     *
-     * @var \Drupal\Core\Cache\CacheBackendInterface
-     */
-    protected $cache;
+  const PROGRESS_TOTAL = 1000;
 
-    /**
-     * @var \EasyRdf_Graph $graph
-     */
-    protected $graph;
+  /**
+   * The cache backend.
+   *
+   * @var \Drupal\Core\Cache\CacheBackendInterface
+   */
+  protected $cache;
 
-    /**
-     * RedirectService constructor.
-     *
-     * @param \Drupal\Core\StringTranslation\TranslationInterface $stringTranslation
-     *   The string translation service.
-     * @param CacheBackendInterface $cache
-     */
-    public function __construct(TranslationInterface $stringTranslation, CacheBackendInterface $cache)
-    {
-        $this->stringTranslation = $stringTranslation;
-        $this->cache = $cache;
-        $this->initializeGraph();
+  /**
+   * @var \EasyRdf_Graph $graph
+   */
+  protected $graph;
+
+  /**
+   * RedirectService constructor.
+   *
+   * @param \Drupal\Core\StringTranslation\TranslationInterface $stringTranslation
+   *   The string translation service.
+   * @param CacheBackendInterface $cache
+   */
+  public function __construct(TranslationInterface $stringTranslation, CacheBackendInterface $cache) {
+    $this->stringTranslation = $stringTranslation;
+    $this->cache = $cache;
+    $this->initializeGraph();
+  }
+
+  protected function initializeGraph() {
+    if (!isset($this->graph)) {
+      $this->graph = new \EasyRdf_Graph();
     }
+  }
 
-    protected function initializeGraph()
-    {
-        if (!isset($this->graph)) {
-            $this->graph = new \EasyRdf_Graph();
-        }
+  /**
+   * @return \EasyRdf_Graph
+   */
+  public function getGraph() {
+    return $this->graph;
+  }
+
+  /**
+   * @param $application_uri Base URI of reste API
+   * @param $vocabularyUri URI of the vocabulary (conceptScheme) to load
+   * @param StateInterface $state
+   *
+   * @return bool
+   */
+  public function fetchRdfData($application_uri, $vocabularyUri, $max, StateInterface $state, FeedInterface $feed, $cacheKey) {
+    if (!$this->loadVocabulary($application_uri, $vocabularyUri, $state)) {
+      return FALSE;
     }
+    $scheme = $this->getGraph()->allOfType('skos:ConceptScheme')[0];
+    $topConcepts = $scheme->all('skos:hasTopConcept');
+    //$state->total = self::PROGRESS_TOTAL;
+    //$state->progress(self::PROGRESS_TOTAL, 0);
+    return $this->loadConceptsRecursively($application_uri, $topConcepts, $max, $cacheKey, [], $state, $feed, self::PROGRESS_TOTAL);
+  }
 
-    /**
-     * @return \EasyRdf_Graph
-     */
-    public function getGraph()
-    {
-        return $this->graph;
+
+  /**
+   * @param $application_uri
+   * @param $vocabulary
+   * @param StateInterface $state
+   *
+   * @return bool
+   */
+  private function loadVocabulary($application_uri, $vocabulary, StateInterface $state) {
+    $completeUri = "$application_uri?uri=$vocabulary&format=text/turtle";
+    try {
+      $this->graph->load($completeUri, 'text/turtle');
+    } catch (\EasyRdf_Http_Exception $e) {
+      $args = [
+        '%uri' => $application_uri,
+        '%resource' => $vocabulary,
+        '%error' => $e->getMessage(),
+      ];
+      $state->setMessage($this->t('Http error while loading data from "%uri" for resource : "%resource" with message: "%error"', $args), 'error');
+      return FALSE;
+    } catch (\EasyRdf_Exception $e) {
+      $args = [
+        '%uri' => $application_uri,
+        '%resource' => $vocabulary,
+        '%error' => $e->getMessage(),
+      ];
+      $state->setMessage($this->t('Unknown error while loading data from "%uri" for resource : "%resource" with message: "%error"', $args), 'error');
+      return FALSE;
     }
+    return TRUE;
+  }
 
-    /**
-     * @param $application_uri Base URI of reste API
-     * @param $vocabularyUri URI of the vocabulary (conceptScheme) to load
-     * @param StateInterface $state
-     * @return bool
-     */
-    public function fetchRdfData($application_uri, $vocabularyUri, $max, StateInterface $state, FeedInterface $feed, $cacheKey)
-    {
-        if (!$this->loadVocabulary($application_uri, $vocabularyUri, $state)) {
-            return FALSE;
-        }
-        $scheme = $this->getGraph()->allOfType('skos:ConceptScheme')[0];
-        $topConcepts = $scheme->all('skos:hasTopConcept');
-        //$state->total = self::PROGRESS_TOTAL;
-        //$state->progress(self::PROGRESS_TOTAL, 0);
-        return $this->loadConceptsRecursively($application_uri, $topConcepts, $max, $cacheKey, [], $state, $feed, self::PROGRESS_TOTAL);
+  /**
+   * @param StateInterface $state
+   *
+   * @return bool|string
+   */
+  public function serialize(StateInterface $state) {
+    $path = __DIR__ . '/../..';
+    set_include_path(get_include_path() . PATH_SEPARATOR . $path);
+    \EasyRdf_Format::registerSerialiser('ntriples', 'EasyRdf_Serialiser_Arc');
+    try {
+      $data = $this->graph->serialise('ntriples');
+    } catch (\Exception $e) {
+      $args = ['%error' => $e->getMessage()];
+      $state->setMessage($this->t('Error while serialising data to turtle with message: "%error"', $args), 'error');
+      return FALSE;
     }
+    return $data;
+  }
 
-
-    /**
-     * @param $application_uri
-     * @param $vocabulary
-     * @param StateInterface $state
-     * @return bool
-     */
-    private function loadVocabulary($application_uri, $vocabulary, StateInterface $state)
-    {
-        $completeUri = "$application_uri?uri=$vocabulary&format=text/turtle";
-        try {
-            $this->graph->load($completeUri, 'text/turtle');
-        } catch (\EasyRdf_Http_Exception $e) {
-            $args = ['%uri' => $application_uri, '%resource' => $vocabulary, '%error' => $e->getMessage()];
-            $state->setMessage($this->t('Http error while loading data from "%uri" for resource : "%resource" with message: "%error"', $args), 'error');
-            return false;
-        } catch (\EasyRdf_Exception $e) {
-            $args = ['%uri' => $application_uri, '%resource' => $vocabulary, '%error' => $e->getMessage()];
-            $state->setMessage($this->t('Unknown error while loading data from "%uri" for resource : "%resource" with message: "%error"', $args), 'error');
-            return false;
-        }
-        return true;
+  public function findConcepts($filepath, StateInterface $state) {
+    if (!is_file($filepath) || !is_readable($filepath)) {
+      throw new \InvalidArgumentException("\$filepath must exist and be readable.");
     }
-
-    /**
-     * @param StateInterface $state
-     * @return bool|string
-     */
-    public function serialize(StateInterface $state)
-    {
-        $path = __DIR__ . '/../..';
-        set_include_path(get_include_path() . PATH_SEPARATOR . $path);
-        \EasyRdf_Format::registerSerialiser('ntriples', 'EasyRdf_Serialiser_Arc');
-        try {
-            $data = $this->graph->serialise('ntriples');
-        } catch (\Exception $e) {
-            $args = ['%error' => $e->getMessage()];
-            $state->setMessage($this->t('Error while serialising data to turtle with message: "%error"', $args), 'error');
-            return false;
-        }
-        return $data;
+    $data = file_get_contents($filepath);
+    $this->graph->parse($data, 'application/n-triples');
+    $conceptSchemes = $this->graph->allOfType('skos:ConceptScheme');
+    if (count($conceptSchemes) < 1) {
+      $args = ['%file' => $filepath];
+      $state->setMessage($this->t('Unable to find ConceptScheme in generated skos file : "%file"', $args), 'error');
+      return FALSE;
     }
-
-    public function findConcepts($filepath)
-    {
-        if (!is_file($filepath) || !is_readable($filepath)) {
-            throw new \InvalidArgumentException("\$filepath must exist and be readable.");
-        }
-        $data = file_get_contents($filepath);
-        $this->graph->parse($data, 'application/n-triples');
-        return $this->graph->allOfType('skos:Concept');
+    else {
+      $conceptScheme = $conceptSchemes[0];
     }
+    $tops = $this->graph->allResources($conceptScheme->getUri(), '^skos:topConceptOf');
+    $children = [];
+    foreach ($tops as $top) {
+      $children = array_merge($children, $this->findNarrowerRecursively($top));
+    }
+    return array_merge($tops, $children);
+  }
 
-    private function loadConceptsRecursively($application_uri, array $resources, $max, $cacheKey, array $buffer, StateInterface $state, FeedInterface $feed, $maxProgress)
-    {
-        if (count($resources) == 0) {
-            return TRUE;
-        }
-        $newConceptFound = FALSE;
-        $progressShare = (float)($maxProgress / count($resources));
-        $counter = 0;
-        foreach ($resources as $resource) {
-            error_log($resource->getUri());
-            if (in_array($resource->getUri(), $buffer)) {
-                continue;
-            }
-            if ($this->isInCache($resource->getUri(), $cacheKey)) {
-                continue;
-            }
+  private function loadConceptsRecursively($application_uri, array $resources, $max, $cacheKey, array $buffer, StateInterface $state, FeedInterface $feed, $maxProgress) {
+    if (count($resources) == 0) {
+      return TRUE;
+    }
+    $newConceptFound = FALSE;
+    $progressShare = (float) ($maxProgress / count($resources));
+    $counter = 0;
+    foreach ($resources as $resource) {
+      error_log($resource->getUri());
+      if (in_array($resource->getUri(), $buffer)) {
+        continue;
+      }
+      if ($this->isInCache($resource->getUri(), $cacheKey)) {
+        //continue;
+      }
 
-            $buffer[] = $resource->getUri();
-            if (count($buffer) >= $max) {
-                return TRUE;
-            }
-            $args = ['%uri' => $resource->getUri()];
-            $state->setMessage($this->t('Loading concept "%uri"', $args), 'status');
-            $state->logMessages($feed);
-            if (!$this->loadVocabulary($application_uri, $resource->getUri(), $state)) {
-                return FALSE;
-            }
-            $newConceptFound = TRUE;
-            //$state->progress(self::PROGRESS_TOTAL, $counter * $progressShare);
-            $counter++;
-            $this->setInCache($resource->getUri(), $cacheKey);
-        }
-        if (TRUE === $newConceptFound) {
-            $children = $resource->all('skos:narrower');
-
-            if (!$this->loadConceptsRecursively($application_uri, $children, $max, $cacheKey, $buffer, $state, $feed, $counter * $progressShare)) {
-                return FALSE;
-            }
-        }
+      $buffer[] = $resource->getUri();
+      if (count($buffer) >= $max) {
         return TRUE;
+      }
+      $args = ['%uri' => $resource->getUri()];
+      $state->setMessage($this->t('Loading concept "%uri"', $args), 'status');
+      $state->logMessages($feed);
+      if (!$this->loadVocabulary($application_uri, $resource->getUri(), $state)) {
+        return FALSE;
+      }
+      $newConceptFound = TRUE;
+      //$state->progress(self::PROGRESS_TOTAL, $counter * $progressShare);
+      $counter++;
+      $this->setInCache($resource->getUri(), $cacheKey);
     }
+    if (TRUE === $newConceptFound) {
+      $children = $resource->all('skos:narrower');
 
-    private function isInCache($uri, $cacheKey)
-    {
-        $cachedUris = $this->getCachedUris($cacheKey);
-        return is_array($cachedUris) && in_array($uri, $cachedUris);
+      if (!$this->loadConceptsRecursively($application_uri, $children, $max, $cacheKey, $buffer, $state, $feed, $counter * $progressShare)) {
+        return FALSE;
+      }
     }
+    return TRUE;
+  }
+
+  private function isInCache($uri, $cacheKey) {
+    $cachedUris = $this->getCachedUris($cacheKey);
+    return is_array($cachedUris) && in_array($uri, $cachedUris);
+  }
 
 
-    private function setInCache($uri, $cacheKey)
-    {
-        $cachedUris = $this->getCachedUris($cacheKey);
-        if (!is_array($cachedUris)) {
-            $cachedUris = [$uri];
-        } else {
-            $cachedUris[] = $uri;
-        }
-        $urisCache = $this->cache->set($cacheKey . '_loaded_uris', $cachedUris);
+  private function setInCache($uri, $cacheKey) {
+    $cachedUris = $this->getCachedUris($cacheKey);
+    if (!is_array($cachedUris)) {
+      $cachedUris = [$uri];
     }
-
-
-    /**
-     * @param $cacheKey
-     * @return bool
-     */
-    private function getCachedUris($cacheKey)
-    {
-        return $cachedUris = $this->cache->get($cacheKey . '_loaded_uris')->data;
+    else {
+      $cachedUris[] = $uri;
     }
+    $urisCache = $this->cache->set($cacheKey . '_loaded_uris', $cachedUris);
+  }
+
+
+  /**
+   * @param $cacheKey
+   *
+   * @return bool
+   */
+  private function getCachedUris($cacheKey) {
+    return $cachedUris = $this->cache->get($cacheKey . '_loaded_uris')->data;
+  }
+
+  /**
+   * @param \EasyRdf_Resource $concept
+   *
+   * @return array
+   */
+  private function findNarrowerRecursively(\EasyRdf_Resource $concept) {
+    $transitiveNarrowerConcept = $narrowerConcepts = $this->graph->allResources($concept->getUri(), 'skos:narrower');
+    foreach ($narrowerConcepts as $child) {
+      $transitiveNarrowerConcept = array_merge($transitiveNarrowerConcept, $this->findNarrowerRecursively($child));
+    }
+    return $transitiveNarrowerConcept;
+  }
 
 }
