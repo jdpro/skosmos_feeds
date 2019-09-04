aaa0ffd371c7eefabed21eb3fafb992e6b5a718e
joachim.dornbusch@ehess.fr <joachim.dornbusch@ehess.fr>
2019-09-04 07:02:55 +0200

    Fix vocabulary label issue in log message

--- src/Feeds/Processor/CustomTaxonomyTermProcessor.php
+++ src/Feeds/Processor/CustomTaxonomyTermProcessor.php
@@ -25,120 +25,115 @@ use Drupal\field\Entity\FieldStorageConfig;
  *     "@entity_type.bundle.info",
  *   },
  *   form = {
- *     "configuration" = "Drupal\feeds\Feeds\Processor\Form\DefaultEntityProcessorForm",
- *     "option" = "Drupal\feeds\Feeds\Processor\Form\EntityProcessorOptionForm",
- *     "feed" = "Drupal\skosmos_feeds\Feeds\Processor\Form\CustomTaxonomyProcessorFeedForm",
+ *     "configuration" =
+ *   "Drupal\feeds\Feeds\Processor\Form\DefaultEntityProcessorForm",
+ *     "option" =
+ *   "Drupal\feeds\Feeds\Processor\Form\EntityProcessorOptionForm",
+ *     "feed" =
+ *   "Drupal\skosmos_feeds\Feeds\Processor\Form\CustomTaxonomyProcessorFeedForm",
  *   },
  * )
  */
-class CustomTaxonomyTermProcessor extends EntityProcessorBase
-{
+class CustomTaxonomyTermProcessor extends EntityProcessorBase {
 
-    public function process(FeedInterface $feed, ItemInterface $item, StateInterface $state)
-    {
-        // Substitute default target vocabulary (defined on feed type level)
-        // by specific one (defined on feed level)
-        if (isset($feed->get('config')->target_vocabulary)) {
-            $this->configuration['values']['vid'] = $feed->get('config')->target_vocabulary;
-        }
-        $this->prepareFeedsItemField();
-        $this->prepareFeedsURIField();
-        $this->prepareFeedsMappings($feed);
-        parent::process($feed, $item, $state);
+  public function process(FeedInterface $feed, ItemInterface $item, StateInterface $state) {
+    $this->overrideDefaultVocabularyId($feed);
+    $this->prepareFeedsItemField();
+    $this->prepareFeedsURIField();
+    $this->prepareFeedsMappings($feed);
+    parent::process($feed, $item, $state);
+  }
+
+  public function postProcess(FeedInterface $feed, StateInterface $state) {
+    // FIXME something erases value in configuration
+    // and makes the default taxonomy label appear in log messages instead of the specific one
+    $this->overrideDefaultVocabularyId($feed);
+    parent::postProcess($feed, $state);
+  }
+
+  protected function prepareFeedsURIField() {
+    // Do not create field when syncing configuration.
+    if (\Drupal::isConfigSyncing()) {
+      return FALSE;
+    }
+    // Create field if it doesn't exist.
+    if (!FieldStorageConfig::loadByName($this->entityType(), 'concept_uri')) {
+      FieldStorageConfig::create([
+        'field_name' => 'concept_uri',
+        'entity_type' => 'taxonomy_term',
+        'type' => 'string',
+        'translatable' => FALSE,
+        'cardinality' => 1,
+      ])->save();
+    }
+    // Create field instance if it doesn't exist.
+    if (!FieldConfig::loadByName($this->entityType(), $this->bundle(), 'concept_uri')) {
+      FieldConfig::create([
+        'label' => $this->t('Concept URI'),
+        'description' => $this->t('URI of the SKOS concept'),
+        'field_name' => 'concept_uri',
+        'field_type' => 'string',
+        'entity_type' => $this->entityType(),
+        'bundle' => $this->bundle(),
+      ])->save();
+    }
+  }
+
+  public function bundleLabel() {
+    return $this->t('Default vocabulary (if not specified in feed options)');
+  }
+
+  private function prepareFeedsMappings(FeedInterface $feed) {
+    $currentMappings = $feed->getType()->getMappingTargets();
+    if (!$this->mappingsContainsTarget('parent', $feed)) {
+      $feed->getType()->addMapping([
+        'target' => 'parent',
+        'map' =>
+          [
+            'target_id' => 'broader',
+          ],
+        'settings' =>
+          [
+            'reference_by' => 'concept_uri',
+            'feeds_item' => 'guid',
+            'autocreate' => 0,
+          ],
+      ]);
+    }
+    if (!$this->mappingsContainsTarget('concept_uri', $feed)) {
+      $feed->getType()->addMapping([
+        'target' => 'concept_uri',
+        'map' =>
+          [
+            'value' => 'URI',
+          ],
+        'unique' =>
+          [
+            'value' => '1',
+          ],
+      ]);
     }
 
-    protected function prepareFeedsURIField()
-    {
-        // Do not create field when syncing configuration.
-        if (\Drupal::isConfigSyncing()) {
-            return FALSE;
-        }
-        // Create field if it doesn't exist.
-        if (!FieldStorageConfig::loadByName($this->entityType(), 'concept_uri')) {
-            FieldStorageConfig::create([
-                'field_name' => 'concept_uri',
-                'entity_type' => 'taxonomy_term',
-                'type' => 'string',
-                'translatable' => FALSE,
-                'cardinality' => 1,
-            ])->save();
-        }
-        // Create field instance if it doesn't exist.
-        if (!FieldConfig::loadByName($this->entityType(), $this->bundle(), 'concept_uri')) {
-            FieldConfig::create([
-                'label' => $this->t('Concept URI'),
-                'description' => $this->t('URI of the SKOS concept'),
-                'field_name' => 'concept_uri',
-                'field_type' => 'string',
-                'entity_type' => $this->entityType(),
-                'bundle' => $this->bundle(),
-            ])->save();
-        }
-    }
+  }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function entityLabel()
-    {
-        return $this->t('Custom taxonomy term');
+  private function mappingsContainsTarget($target, FeedInterface $feed) {
+    $currentMappings = $feed->getType()->getMappings();
+    foreach ($currentMappings as $currentMapping) {
+      if (isset($currentMapping['target']) && $currentMapping['target'] == $target) {
+        return TRUE;
+      }
     }
+    return FALSE;
+  }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function entityLabelPlural()
-    {
-        return $this->t('Custom vocabulary terms');
+  /**
+   * @param \Drupal\feeds\FeedInterface $feed
+   */
+  private function overrideDefaultVocabularyId(FeedInterface $feed) {
+    // Substitute default target vocabulary (defined on feed type level)
+    // by specific one (defined on feed level)
+    if (isset($feed->get('config')->target_vocabulary)) {
+      $this->configuration['values']['vid'] = $feed->get('config')->target_vocabulary;
     }
-
-    public function bundleLabel()
-    {
-        return $this->t('Default vocabulary (if not specified in feed options)');
-    }
-
-    private function prepareFeedsMappings(FeedInterface $feed)
-    {
-        $currentMappings = $feed->getType()->getMappingTargets();
-        if(!$this->mappingsContainsTarget('parent', $feed)){
-            $feed->getType()->addMapping(array(
-                'target' => 'parent',
-                'map' =>
-                    array(
-                        'target_id' => 'broader',
-                    ),
-                'settings' =>
-                    array(
-                        'reference_by' => 'concept_uri',
-                        'feeds_item' => 'guid',
-                        'autocreate' => 0,
-                    ),
-            ));
-        }
-        if(!$this->mappingsContainsTarget('concept_uri', $feed)){
-            $feed->getType()->addMapping(array(
-                'target' => 'concept_uri',
-                'map' =>
-                    array(
-                        'value' => 'URI',
-                    ),
-                'unique' =>
-                    array(
-                        'value' => '1',
-                    ),
-            ));
-        }
-
-    }
-
-    private function mappingsContainsTarget($target, FeedInterface $feed)
-    {
-        $currentMappings = $feed->getType()->getMappings();
-        foreach ($currentMappings as $currentMapping) {
-            if (isset($currentMapping['target']) && $currentMapping['target'] == $target) {
-                return true;
-            }
-        }
-        return false;
-    }
+  }
 }
